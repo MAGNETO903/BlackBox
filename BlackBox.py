360651caa962c52fa6eac1f54f68b7ec02256170
Danil <cherkashin.daniel@gmail.com>
2021-02-20 17:22:40 +0700

    added config file added run_bot.py

---
+++ BlackBox.py
@@ -0,0 +1,11 @@
+class BlackBox():
+    def __init__(self):
+        pass
+
+    def make_transformation(self, user_try):
+        if user_try.isdigit():
+            return 'Введите пожалуйста 9-значное число'
+        elif len(user_try) != 9:
+            return 'Пожалуйста, введите число, состоящее ровно из 9 цифр'
+        else:
+            return 'Пока не знаю, что ответить...'
--- run_bot.py
+++ run_bot.py
@@ -1,3 +1,18 @@
 import telebot
 
+import Controller
+import BlackBox
+
 from configuration import API_TOKEN
+
+bot = telebot.TeleBot(API_TOKEN)
+black_box = BlackBox.BlackBox()
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.from_user.id, "Здравствуйте!")
+
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    answer = black_box.make_transformation(message)
+    bot.send_message(message.from_user.id, answer)
