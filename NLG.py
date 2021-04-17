d9d9aad17bf746dea2af7f28720fd5da03606966
Danil <cherkashin.daniel@gmail.com>
2021-04-17 16:38:46 +0700

    added get_message method to nlg generator

--- NLG.py
+++ NLG.py
@@ -12,3 +12,15 @@ class NLGMeta(type):
 class NLG(metaclass=NLGMeta):
     def __init__(self):
         pass
+
+    def get_message(self, user_status, pic):
+        if user_status == 0:
+            # отправляем картинку
+            return pic
+        elif user_status == 1:
+            # введите именно 9-значное число
+            return "Пожалуйста введите код, состоящий ровно из 9 цифр"
+        elif user_status == 2:
+            # введите код, состоящий только из 9 цифр
+            return "Введите код, состоящий только из 9 цифр"
+
